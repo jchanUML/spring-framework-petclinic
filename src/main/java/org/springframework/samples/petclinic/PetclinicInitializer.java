7b8bab2c83d3f051dacfea06cee4b8698aa53427
jchanUML <47180203+jchanUML@users.noreply.github.com>
2019-12-16 17:02:13 +0100

    Update PetclinicInitializer.java

--- src/main/java/org/springframework/samples/petclinic/PetclinicInitializer.java
+++ src/main/java/org/springframework/samples/petclinic/PetclinicInitializer.java
@@ -77,15 +77,4 @@ public class PetclinicInitializer extends AbstractDispatcherServletInitializer {
         CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter("UTF-8", true);
         return new Filter[]{characterEncodingFilter};
     }
-    
-    @Controller
-    @SessionAttributes("hello")  // Noncompliant; this doesn't get cleaned up
-    public class HelloWorld {
-
-      @RequestMapping("/greet", method = GET)
-      public String greet(String greetee) {
-
-        return "Hello " + greetee;
-      }
-    }
 }
